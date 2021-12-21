983eff169bd0295959e1d3af6dc2f52a32c797f7
OrhanBakar <obkr4206@gmail.com>
2021-12-21 17:10:26 +0100

    Added more work

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("work1 started");
         System.out.println("Created Git Repository");
+        System.out.println("Made few commit");
 
     }
 
