85ce8cfed60cde8228d6e60080df4cbabda58b54
OrhanBakar <obkr4206@gmail.com>
2021-12-26 13:27:56 +0100

    Check out note is created

--- src/work1/CheckOutPage.java
+++ src/work1/CheckOutPage.java
@@ -3,5 +3,6 @@ package work1;
 public class CheckOutPage {
     public static void main(String[] args) {
         System.out.println("Page is required to be checked out");
+        System.out.println("conflict1");
     }
 }
